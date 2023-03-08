cb40a53daa814f8e4fa01fe818319b8979178150
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-03-08 13:03:38 +0100

    added styles components with good file architecture

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,81 +1,95 @@
 import React, { useState } from "react";
-import style from "./style.css";
+import Button from "../../styles/components/Button.js";
 import axios from "axios";
 import qs from "qs";
+import styled from "styled-components";
+
+// Create a styled form
+const Form = styled.form`
+  background-color: none;
+  color: grey;
+  font-size: 1.5rem;
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 function Login() {
-	const [userName, setUserName] = useState("");
-	const [mail, setMail] = useState("");
-	const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const handleSubmit = async (event) => {
-		const data = {
-			userName,
-			mail,
-			password,
-		};
+  const handleSubmit = async (event) => {
+    const data = {
+      userName,
+      mail,
+      password,
+    };
 
-		const headers = {
-			"Content-Type": "application/x-www-form-urlencoded",
-		};
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
 
-		console.log(userName, mail, password);
-		event.preventDefault();
+    console.log(userName, mail, password);
+    event.preventDefault();
 
-		try {
-			const response = await axios.post(
-				"http://localhost:4000/users",
-				qs.stringify(data),
-				{
-					headers,
-				}
-			);
-			const message = response.data.message;
-			console.log(message);
-		} catch (error) {
-			console.log(error);
-			alert("Erreur lors de la création de l'utilisateur");
-		}
-	};
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/users",
+        qs.stringify(data),
+        {
+          headers,
+        }
+      );
+      const message = response.data.message;
+      console.log(message);
+    } catch (error) {
+      console.log(error);
+      alert("Erreur lors de la création de l'utilisateur");
+    }
+  };
 
-	// console.log("token");
+  // console.log("token");
 
-	return (
-		<>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Pseudo :
-					<input
-						type="text"
-						value={userName}
-						onChange={(e) => setUserName(e.target.value)}
-					/>
-				</label>
-				<label>
-					mail :
-					<input
-						type="text"
-						value={mail}
-						onChange={(e) => setMail(e.target.value)}
-					/>
-				</label>
-				<label>
-					pwd :
-					<input
-						type="text"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</label>
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <label>
+          Pseudo :
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </label>
+        <label>
+          mail :
+          <input
+            type="text"
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+          />
+        </label>
+        <label>
+          pwd :
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
 
-				<button type="submit">Créer un compte</button>
-			</form>
-			<p>
-				Already have an account ?{" "}
-				<a href="http://localhost:4000/login">Login</a>{" "}
-			</p>
-		</>
-	);
+        <Button type="submit">Créer un compte</Button>
+      </Form>
+      <p>
+        Already have an account ?{" "}
+        <a href="http://localhost:4000/login">Login</a>{" "}
+      </p>
+    </>
+  );
 }
 
 export default Login;
---
+++ src/styles/components/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+// Create a styled component
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+  font-size: 1rem;
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+`;
+
+export default Button;
