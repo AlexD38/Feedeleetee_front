034e5b8b6ee7fef84870dba23e914c3f7a859da9
AlexD38 <dartiguelongue.alexis@gmail.com>
2023-04-04 22:04:45 +0200

    client dashboard fuzzy search for enterprises

--- src/components/ClientDashboard.jsx
+++ src/components/ClientDashboard.jsx
@@ -1,22 +1,31 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Card from "../styles/components/card";
+import Greetings from "../styles/components/Greetings";
 import HorizontalWrapper from "../styles/components/HorizontalWrapper";
 import InteractiveCard from "../styles/components/interactiveCard";
 import Logo from "../styles/components/Logo.js";
 import AppointmentsInfos from "./enterepriseDashboard/appointmentsInfos/appointmentsInfos";
 import Modal from "./Modal/Modal";
+import moment from "moment";
+import "moment/locale/fr";
+import Input from "../styles/components/input";
+import SideBar from "../styles/layout/sideBar.js";
+import VerticalWrapper from "../styles/components/verticalWrapper.js";
 
 export function ClientDashboard() {
 	const token = localStorage.getItem("token");
-	const Navigate = useNavigate();
+	const [user, setUser] = useState(localStorage.getItem("user"));
+	const [client, setClient] = useState([]);
 	const [enterprises, setEnterprises] = useState([]);
-	const [showAppointmens, setshowAppointments] = useState(false);
-	const [availableAppointments, setAvailableAppointments] = useState([]);
+	const Navigate = useNavigate();
+	const inputRef = useRef(null);
+
 	const [clientId, setClientId] = useState(localStorage.getItem("clientId"));
+	const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
 	useEffect(() => {
-		async function fetchEnterprises() {
+		async function fetchClient() {
 			const headers = {
 				token: token,
 				clientId: +clientId,
@@ -30,6 +39,7 @@ export function ClientDashboard() {
 				console.log(response.status);
 				if (response.status === 200) {
 					console.log(response.data[0]);
+					setClient((client) => response.data[0]);
 				} else if (clientId) {
 					localStorage.removeItem("clientId");
 				} else {
@@ -39,52 +49,77 @@ export function ClientDashboard() {
 				console.log(error);
 			}
 		}
-		fetchEnterprises();
+		fetchClient();
+		setDate(client.rdv);
 	}, []);
-	const handleclick = async (e) => {
-		if (!e.target.id) {
-			console.log("no target id");
-			return;
+	const handleChange = (e) => {
+		if (inputRef.current.value.length > 0) {
+			console.log(inputRef.current.value);
+			console.log(inputRef.current.value.length);
+			const fetchEnterprise = async () => {
+				const headers = {
+					token: token,
+				};
+				try {
+					const response = await axios.get(
+						"http://localhost:4000/enterprises",
+						{ headers }
+					);
+					// console.log(response.data.enterprises);
+					setEnterprises((enterprise) => response.data.enterprises);
+					console.log(enterprises);
+				} catch (error) {
+					console.log(error);
+				}
+			};
+			fetchEnterprise();
 		}
-		localStorage.setItem("enterpriseId", e.target.id);
-		const enterpriseId = localStorage.getItem("enterpriseId");
-		if (!enterpriseId) {
-			console.log("no id found");
-			return;
+		if (inputRef.current.value.length < 1) {
+			setEnterprises([]);
+			// console.log("whattttttttttttttttttttttttt");
 		}
-		Navigate("/takeAppointment");
 	};
+
 	return (
 		<>
-			<h1>Hello welcome to your client dashboard</h1>
-			<HorizontalWrapper>
-				{enterprises ? (
-					<HorizontalWrapper>
-						{" "}
-						{enterprises.map((enterprise) => (
-							<InteractiveCard
-								onClick={handleclick}
-								id={enterprise.id}
-								style={{ cursor: "pointer" }}>
-								<>
-									{enterprise.logo && (
-										<Logo
-											style={{
-												position: "relative",
-												marginBottom: "3rem",
-											}}
-											src={`data:image/png;base64,${enterprise.logo}`}></Logo>
-									)}
-									<h1>{enterprise.name}</h1>
-									<h4>{enterprise.description}</h4>
-								</>
-							</InteractiveCard>
-						))}
-					</HorizontalWrapper>
-				) : (
-					<></>
-				)}
-			</HorizontalWrapper>
+			<Greetings>Bonjour {client.firstname}, </Greetings>
+			<h1>Bienvenue sur votre espace client.</h1>
+			<VerticalWrapper>
+				<label htmlFor="searchbar">Chercher un entreprise : </label>
+				<Input
+					ref={inputRef}
+					onChange={handleChange}
+					id="searchbar"
+					placeholder="chercher une entreprise"
+				/>
+			</VerticalWrapper>
+			{enterprises.length > 0 && (
+				<>
+					{enterprises.map((enterprise) => (
+						<Card>{enterprise.name}</Card>
+					))}
+				</>
+			)}
+
+			<SideBar>
+				<>
+					{client.rdv && client.rdv.length > 0 ? (
+						<ul>
+							{" "}
+							<h1>Vos Rendez-vous :</h1>
+							{client.rdv.map((rdv, index) => (
+								<li key={index}>
+									{moment(rdv)
+										.locale("fr")
+										.format("dddd DD/MM/YYYY")}
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>Aucun rendez-vous pour le moment</p>
+					)}
+				</>
+			</SideBar>
 		</>
 	);
 }
