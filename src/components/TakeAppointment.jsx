86c677fb2480727bfccdd97e8bb88cc109ee29b2
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-04-06 16:04:19 +0200

    take appointment fixed

--- src/components/TakeAppointment.jsx
+++ src/components/TakeAppointment.jsx
@@ -6,87 +6,84 @@ import HorizontalWrapper from "../styles/components/HorizontalWrapper";
 import moment from "moment";
 import "moment/locale/fr";
 import CloseBtn from "../styles/components/CloseBtn.js";
+import Card from "../styles/components/card";
+import LinkComp from "../styles/components/LinkComp";
 
 export function TakeAppointment(props) {
-	const token = localStorage.getItem("token");
-	let enterpriseId = localStorage.getItem("enterpriseId") || props.id;
+  const token = localStorage.getItem("token");
+  let enterpriseId = localStorage.getItem("enterpriseId") || props.id;
+  const [showBtns, setShowBtns] = useState(false);
 
-	const [availableAppointments, setAvailableAppointments] = useState([]);
-	const [selectedAppointmentId, setSelectedAppointmentId] = useState(null);
-	const [clientId, setClientId] = useState(props.clientId);
+  const [availableAppointments, setAvailableAppointments] = useState([]);
+  const [showAppointments, setShowAppointments] = useState(false);
+  const [selectedAppointmentId, setSelectedAppointmentId] = useState(null);
+  const [clientId, setClientId] = useState(props.clientId);
 
-	useEffect(() => {
-		async function fetchAvailableAppointmentsFromEnterprise() {
-			const headers = {
-				token: token,
-			};
-			try {
-				const response = await axios.get(
-					`http://localhost:4000/enterprises/${enterpriseId}/appointments`,
-					{ headers }
-				);
-				console.log(response.data);
-				setAvailableAppointments(response.data);
-			} catch (error) {
-				console.error(error);
-				// handle error
-			}
-		}
-		fetchAvailableAppointmentsFromEnterprise();
-	}, []);
+  useEffect(() => {
+    async function fetchAvailableAppointmentsFromEnterprise() {
+      const headers = {
+        token: token,
+      };
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/enterprises/${enterpriseId}/appointments`,
+          { headers }
+        );
+        console.log(response.data);
+        setAvailableAppointments(response.data);
+      } catch (error) {
+        console.error(error);
+        // handle error
+      }
+    }
+    fetchAvailableAppointmentsFromEnterprise();
+  }, []);
 
-	const handleClick = (appointmentId) => {
-		console.log(`would you like to take appointment id#${appointmentId}?`);
-		setSelectedAppointmentId(appointmentId);
-	};
+  const handleConfirm = (appointmentId) => {
+    console.log(`confirming appointment id#${appointmentId}`);
+    const InsertClientIntoAppointment = async () => {
+      try {
+        const headers = {
+          token: token,
+        };
+        const response = await axios.patch(
+          `http://localhost:4000/appointments/${appointmentId}`,
+          {},
+          { headers }
+        );
 
-	const handleConfirm = () => {
-		console.log(`confirming appointment id#${selectedAppointmentId}`);
-		const InsertClientIntoAppointment = async () => {
-			try {
-				const headers = {
-					token: token,
-				};
-				const response = await axios.patch(
-					`http://localhost:4000/appointments/${selectedAppointmentId}`,
-					{},
-					{ headers }
-				);
-				setSelectedAppointmentId(null);
-				console.log(response);
-			} catch (error) {
-				console.error(error);
-				// handle error
-			}
-		};
-		InsertClientIntoAppointment();
-		window.location.reload();
-	};
+        alert("Votre rendez-vous à bien été pris");
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+        // handle error
+      }
+    };
+    InsertClientIntoAppointment();
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  };
 
-	return (
-		<>
-			{availableAppointments.map((appointment) => (
-				<InteractiveCard
-					key={appointment.id}
-					onClick={() => handleClick(appointment.id)}>
-					<ul id={appointment.id}>
-						<li>
-							{" "}
-							{moment(appointment.day)
-								.locale("fr")
-								.format("dddd DD/MM/YYYY")}
-						</li>
-						<li>{appointment.time_of_day}</li>
-					</ul>
-					{selectedAppointmentId === appointment.id && (
-						<HorizontalWrapper>
-							<Button onClick={handleConfirm}>Confirm</Button>
-							<Button>Cancel</Button>
-						</HorizontalWrapper>
-					)}
-				</InteractiveCard>
-			))}
-		</>
-	);
+  return (
+    <>
+      {availableAppointments.map((appointment) => (
+        <LinkComp
+          key={appointment.id}
+          onClick={() => handleConfirm(appointment.id)}
+        >
+          <ul id={appointment.id}>
+            <li>
+              {" "}
+              {moment(appointment.day)
+                .locale("fr")
+                .format("dddd DD/MM/YYYY")}{" "}
+              {appointment.time_of_day}
+            </li>
+          </ul>
+        </LinkComp>
+      ))}
+    </>
+  );
 }
 // export default TakeAppointment;
--- src/styles/components/interactiveCard.js
+++ src/styles/components/interactiveCard.js
@@ -3,18 +3,20 @@ import Card from "./card";
 
 // Create a styled component
 const InteractiveCard = styled(Card)`
-	position: relative;
-	height: 15rem;
-	width: 15rem;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	&:hover {
-		transform: scale(0.97);
-		transition: all 0.1s ease;
-		cursor: pointer;
-	}
+  position: relative;
+  min-height: 15rem;
+  max-height: fit content;
+  min-width: 15rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  &:hover {
+    transform: scale(0.97);
+    transition: all 0.1s ease;
+    cursor: pointer;
+  }
 `;
 
 export default InteractiveCard;
