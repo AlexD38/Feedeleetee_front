a0109ac7c3ba7c2ffffbc2e9f4080e57fba563b0
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-04-27 10:51:04 +0200

    images choice for home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,69 +1,73 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-
 import Link from "../../styles/components/LinkComp";
-
 import VerticalWrapper from "../../styles/components/verticalWrapper.js";
 import Greetings from "../../styles/components/Greetings.js";
 import Logout from "../Logout/Logout";
 import Card from "../../styles/components/card";
+import EnterpriseChoiceImg from "../../styles/components/EnterpriseChoiceImg.js";
 
 function Home() {
-	const userName = localStorage.getItem("user");
-	const [token, setToken] = useState(localStorage.getItem("token"));
-	const navigate = useNavigate();
+  const userName = localStorage.getItem("user");
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		if (!token) {
-			alert("please login first");
-			navigate("/login");
-		}
-	}, [token]);
+  useEffect(() => {
+    if (!token) {
+      alert("please login first");
+      navigate("/login");
+    }
+  }, [token]);
 
-	return userName ? (
-		<>
-			<VerticalWrapper>
-				<Link href="http://localhost:3000/myenterprise">
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						transition={{ duration: 0.5, delay: 0.1 }}>
-						<Greetings>Hello {userName}</Greetings>
-					</motion.div>
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						whileHover={{ scale: 1.1 }}
-						transition={{
-							type: "spring",
-							stiffness: 400,
-							damping: 10,
-						}}>
-						<Card>Accéder à mon entreprise</Card>
-					</motion.div>
-				</Link>
-				<Link href="http://localhost:3000/myclientprofile">
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						whileHover={{ scale: 1.1 }}
-						transition={{
-							type: "spring",
-							stiffness: 400,
-							damping: 10,
-						}}>
-						<Card>Accéder à mon profil client</Card>
-					</motion.div>
-				</Link>
-				<Logout />
-			</VerticalWrapper>
-		</>
-	) : (
-		<>
-			<h1>nothing to see here...</h1>
-		</>
-	);
+  return userName ? (
+    <>
+      <VerticalWrapper>
+        <Link href="http://localhost:3000/myenterprise">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+          >
+            <Greetings>Hello {userName}</Greetings>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{
+              type: "spring",
+              stiffness: 400,
+              damping: 10,
+            }}
+          >
+            <EnterpriseChoiceImg src="../styles/images/EnterpriseChoiceImg.jpg">
+              Accéder à mon entreprise
+            </EnterpriseChoiceImg>
+          </motion.div>
+        </Link>
+        <Link href="http://localhost:3000/myclientprofile">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{
+              type: "spring",
+              stiffness: 400,
+              damping: 10,
+            }}
+          >
+            <Card>Accéder à mon profil client</Card>
+          </motion.div>
+        </Link>
+        <Logout />
+      </VerticalWrapper>
+    </>
+  ) : (
+    <>
+      <h1>nothing to see here...</h1>
+    </>
+  );
 }
 
 export default Home;
---
+++ src/styles/components/EnterpriseChoiceImg.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+const EnterpriseChoiceImg = styled.img`
+  position: relative;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  max-width: 100%;
+  object-fit: cover;
+
+  @media (max-width: 1400px) {
+    height: 100vh;
+  }
+`;
+
+export default EnterpriseChoiceImg;
