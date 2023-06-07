7b56b13a97de46c97e804b232f1c50dcdbc74f9b
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-06-07 14:10:20 +0200

    fixed redirect after bas signup and added border red to input if id are wrong

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -11,96 +11,110 @@ import Greetings from "../../styles/components/Greetings.js";
 import PropTypes from "prop-types";
 
 function LoginForm(props) {
-  console.log(props);
-  const [mail, setMail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  localStorage.clear();
+    console.log(props);
+    const [mail, setMail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    localStorage.clear();
+    const [inputError, setInputError] = useState(false);
 
-  const handleSubmit = async (event) => {
-    const data = {
-      mail,
-      password,
-    };
+    const handleSubmit = async (event) => {
+        const data = {
+            mail,
+            password,
+        };
 
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
+        const headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+        };
 
-    console.log(data);
-    event.preventDefault();
+        console.log(data);
+        event.preventDefault();
 
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/login",
-        qs.stringify(data),
-        {
-          headers,
+        try {
+            const response = await axios.post(
+                "http://localhost:4000/login",
+                qs.stringify(data),
+                {
+                    headers,
+                }
+            );
+            console.log("reponse : ", response.data);
+
+            if (response.data.error) {
+                alert(response.data.error);
+                setInputError(true);
+                inputErrDisplay();
+                return;
+            }
+            response.data.token = localStorage.setItem(
+                "token",
+                response.data.token
+            );
+            response.data.userName = localStorage.setItem(
+                "user",
+                response.data.userName
+            );
+
+            navigate("/home");
+        } catch (error) {
+            console.log(error);
+            alert("erreur lors de la connexion");
         }
-      );
-      console.log("reponse : ", response.data);
+    };
+    const showPwd = (event) => {
+        event.target.type = "text";
+    };
+    const hidePwd = (event) => {
+        event.target.type = "password";
+    };
+    const inputErrDisplay = (event) => {
+        if (inputError) {
+            event.target.style.border = "3px solid red";
+            console.log(event.target.style);
+            console.log(inputError);
+        }
+    };
 
-      if (response.data.error) {
-        alert(response.data.error);
-        navigate("/login");
-        return;
-      }
-      response.data.token = localStorage.setItem("token", response.data.token);
-      response.data.userName = localStorage.setItem(
-        "user",
-        response.data.userName
-      );
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                <Greetings size="2rem">Identifiez-vous</Greetings>
 
-      navigate("/home");
-    } catch (error) {
-      console.log(error);
-      alert("erreur lors de la connexion");
-    }
-  };
-  const showPwd = (event) => {
-    event.target.type = "text";
-  };
-  const hidePwd = (event) => {
-    event.target.type = "password";
-  };
+                <label>e-mail :</label>
+                <Input
+                    required
+                    type="email"
+                    value={mail}
+                    pattern='^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
+                    onChange={(e) => setMail(e.target.value)}
+                    onMouseEnter={inputErrDisplay}
+                />
+                <label>Mot de passe :</label>
+                <Input
+                    onBlur={hidePwd}
+                    onDoubleClick={showPwd}
+                    required
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    onMouseEnter={inputErrDisplay}
+                />
 
-  return (
-    <>
-      <Form onSubmit={handleSubmit}>
-        <Greetings size="2rem">Identifiez-vous</Greetings>
-
-        <label>e-mail :</label>
-        <Input
-          required
-          type="email"
-          value={mail}
-          pattern='^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
-          onChange={(e) => setMail(e.target.value)}
-        />
-        <label>Mot de passe :</label>
-        <Input
-          onBlur={hidePwd}
-          onDoubleClick={showPwd}
-          required
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <Button type="submit">Me connecter</Button>
-        <VerticalWrapper>
-          <p>
-            Don't have an account yet ?{" "}
-            <Link onClick={props.changeDisplay}>Create One</Link>{" "}
-          </p>
-        </VerticalWrapper>
-      </Form>
-    </>
-  );
+                <Button type="submit">Me connecter</Button>
+                <VerticalWrapper>
+                    <p>
+                        Don't have an account yet ?{" "}
+                        <Link onClick={props.changeDisplay}>Create One</Link>{" "}
+                    </p>
+                </VerticalWrapper>
+            </Form>
+        </>
+    );
 }
 LoginForm.propTypes = {
-  mail: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
+    mail: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
 };
 
 export default LoginForm;
